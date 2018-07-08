2463afe1807df66e2e33105bb2063f66ea1d3327
Shrimant More <smshrimant@gmail.com>
2018-07-08 22:51:22 +0530

    New file added

---
+++ displaydate.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<body>
+    
+   	<h3> My first javascript script :) </h3>
+    
+      <button type="button"
+      onclick= "document.getElementById('demo').innerHTML=Date()">
+      Press to get Date and Time
+      </button>
+        
+    <p id="demo"> hi</p>
+  </body>
+</html>
